d3a0ad02b7ece0365c73036673478db606aea4f3
Ishana Narayanan <ishana_narayanan@ucsb.edu>
2020-04-26 18:26:12 -0700

    in/cb - added text to home page

--- components/AppFooter.js
+++ components/AppFooter.js
@@ -3,8 +3,11 @@ import Container from "react-bootstrap/Container";
 function AppFooter() {
   return (
     <Container>
-      This is a demo of a minimal Next.js app. Check out the source code on{" "}
-      <a href="https://github.com/ucsb-cs48-s20/demo-nextjs-app">GitHub</a>!
+      <div class="footer">
+        <p>Check out the source code on </p>
+        <a href="https://github.com/ucsb-cs48-s20/project-s2-t2-free">GitHub</a>
+        !
+      </div>
     </Container>
   );
 }
