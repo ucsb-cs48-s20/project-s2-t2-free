ace57fa4a239c0d527220955a58a4bd6c8ba18cb
Ishana Narayanan <ishana_narayanan@ucsb.edu>
2020-04-26 18:32:00 -0700

    in/cb - fixed compile error

--- components/AppFooter.js
+++ components/AppFooter.js
@@ -3,11 +3,8 @@ import Container from "react-bootstrap/Container";
 function AppFooter() {
   return (
     <Container>
-      <div class="footer">
-        <p>Check out the source code on </p>
-        <a href="https://github.com/ucsb-cs48-s20/project-s2-t2-free">GitHub</a>
-        !
-      </div>
+      Check out the source code on{" "}
+      <a href="https://github.com/ucsb-cs48-s20/project-s2-t2-free">GitHub</a>!
     </Container>
   );
 }
