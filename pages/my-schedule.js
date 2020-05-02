c1fdfd07a0a30baedca6136b54c5596fcf82ae12
Corey Su <coreysu@ucsb.edu>
2020-05-02 16:54:08 -0700

    cs/in - Added template for adding events to schedule

--- pages/my-schedule.js
+++ pages/my-schedule.js
@@ -1,22 +1,65 @@
-// import useSWR from "swr";
-// import Spinner from "react-bootstrap/Spinner";
-// import Image from "react-bootstrap/Image";
-// import { fetch } from "../utils/fetch";
 import Layout from "../components/Layout";
 import { requiredAuth } from "../utils/ssr";
+import { useCallback, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Head from "next/head";
 
 export const getServerSideProps = requiredAuth;
 
-function SchedulePage(props) {
-  const user = props.user;
+export default function ManageDefaultSchedulePage(props) {
+  const { user, initialData } = props;
+  const [newEventName, setNewEventName] = useState("");
+  const [newDay, setNewDay] = useState("");
+  const [newStartTime, setNewStartTime] = useState("");
+  const [newEndTime, setNewEndTime] = useState("");
+  const addEvent = useCallback(async (e) => {
+    // override default form submission behavior
+    e.preventDefault();
+    e.stopPropagation();
+
+    setNewEventName("");
+    setNewDay("");
+    setNewStartTime("");
+    setNewEndTime("");
+  });
 
   return (
     <Layout user={user}>
-      <div>
-        <p>My Schedule Page</p>
-      </div>
+      <Head>
+        <title>Edit Schedule</title>
+      </Head>
+      <h1>Edit Schedule</h1>
+      <Form onSubmit={addEvent} className="mb-5">
+        <Form.Group>
+          <Form.Label>Event Name</Form.Label>
+          <FormControl
+            type="text"
+            value={newEventName}
+            onChange={(e) => setNewEventName(e.target.value)}
+          />
+          <Form.Label>Day</Form.Label>
+          <FormControl
+            type="text"
+            value={newDay}
+            onChange={(e) => setNewDay(e.target.value)}
+          />
+          <Form.Label>Start Time</Form.Label>
+          <FormControl
+            type="text"
+            value={newStartTime}
+            onChange={(e) => setNewStartTime(e.target.value)}
+          />
+          <Form.Label>End Time</Form.Label>
+          <FormControl
+            type="text"
+            value={newEndTime}
+            onChange={(e) => setNewEndTime(e.target.value)}
+          />
+        </Form.Group>
+        <Button type="submit">Add Event</Button>
+      </Form>
     </Layout>
   );
 }
-
-export default SchedulePage;
