f7afa644ccd8c30422808e92adc39a783002cd72
Vincent Tieu <vktieu@Vincents-MacBook-Pro.local>
2020-05-06 19:09:19 -0700

    vt - cleaned a TON of code

--- pages/my-schedule/index.js
+++ pages/my-schedule/index.js
@@ -1,30 +1,9 @@
 import Layout from "../../components/Layout";
-// import { requiredAuth } from "../../utils/ssr";
+import { requiredAuth } from "../../utils/ssr";
 import NewEventForm from "./NewEventForm";
-import { getEvents } from "../api/event";
-import auth0 from "../../utils/auth0";
-import { serializeDocument } from "../../utils/mongodb";
 import useSWR from "swr";
 
-export const getServerSideProps = async ({ req, res }) => {
-  const session = await auth0.getSession(req);
-  if (session && session.user) {
-    const initialData = (await getEvents(session.user.sub)).map(
-      serializeDocument
-    );
-    return {
-      props: {
-        user: session.user,
-        initialData: initialData,
-      },
-    };
-  }
-
-  res.writeHead(302, {
-    Location: "/api/login",
-  });
-  res.end();
-};
+export const getServerSideProps = requiredAuth;
 
 function SchedulePage(props) {
   const user = props.user;
@@ -34,7 +13,6 @@ function SchedulePage(props) {
       <h1>Create New Event</h1>
       <NewEventForm />
       <h1>My Schedule</h1>
-
       <pre>{JSON.stringify(data, null, "\t")}</pre>
     </Layout>
   );
