fd357a2a3a846789af9096e00afbfe702735394a
Vincent Tieu <vktieu@Vincents-MacBook-Pro.local>
2020-05-06 18:31:14 -0700

    team - cleaned up code

--- pages/api/groups/index.js
+++ pages/api/groups/index.js
@@ -1,4 +1,3 @@
-import validate from "validate.js";
 import { authenticatedAction } from "../../../utils/api";
 import { initDatabase } from "../../../utils/mongodb";
 
@@ -8,66 +7,10 @@ export async function getGroupEvents() {
   return events.find({}).toArray();
 }
 
-const eventConstraints = {
-  eventname: {
-    presence: true,
-  },
-  day: {
-    presence: true,
-  },
-  starttime: {
-    presence: true,
-  },
-  endtime: {
-    presence: true,
-  },
-};
-
-async function createEvent(req, user, res) {
-  let event;
-
-  try {
-    event = await validate.async(req.body, eventConstraints, {
-      cleanAttributes: true,
-      format: "flat",
-    });
-  } catch (err) {
-    throw {
-      status: 400,
-      message: err.join(", "),
-    };
-  }
-
-  console.log("inserting into events:", event);
-
-  const client = await initDatabase();
-  const events = client.collection("events");
-
-  const query = { eventname: "always insert event" };
-  const mutation = {
-    $setOnInsert: {
-      userid: user.sub,
-      eventname: event.eventname,
-      eventday: event.day,
-      eventstarttime: event.starttime,
-      eventendtime: event.endtime,
-    },
-  };
-
-  const result = await events.findOneAndUpdate(query, mutation, {
-    upsert: true,
-    returnOriginal: false,
-  });
-
-  return result.value;
-}
-
 async function performAction(req, user) {
   switch (req.method) {
     case "GET":
       return getGroupEvents();
-    /* case "POST":
-      return createEvent(req, user); */
   }
 
   throw { status: 405 };
