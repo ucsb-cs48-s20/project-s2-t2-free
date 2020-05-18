14646da8a8c965bdaf765f5730fd6a964d31e971
Ishana Narayanan <ishana_narayanan@ucsb.edu>
2020-05-18 15:06:08 -0700

    in - free times print on screen

--- pages/my-schedule/FreeTime.js
+++ pages/my-schedule/FreeTime.js
@@ -1,5 +1,5 @@
-//import useSWR from "swr";
-//import { useCallback, useState } from "react";
+import useSWR from "swr";
+import { render } from "react-dom";
 
 // converts time hh:mm AM/PM to minute of the day
 function convertTime(str) {
@@ -34,7 +34,7 @@ function makeBusy() {
 
 // populates busy with events
 function convertEvent(events) {
-  busy = makeBusy();
+  var busy = makeBusy();
   var len = events.length;
   for (let i = 0; i < len; i++) {
     var start_time = convertTime(events[i][0]);
@@ -48,7 +48,7 @@ function convertEvent(events) {
 }
 
 function makeFree(busy_times) {
-  free_times = [];
+  var free_times = [];
   for (let i = 0; i < 1436; i++) {
     if (busy_times[i] == true) {
       free_times.push(i);
@@ -89,8 +89,8 @@ function makeTimesDict() {
 }
 
 function freeIntervals(free_times) {
-  free_intervals = [];
-  start = free_times[0];
+  var free_intervals = [];
+  var start = free_times[0];
   free_intervals.push(start);
   var i = 1;
   while (i < free_times.length) {
@@ -108,12 +108,12 @@ function freeIntervals(free_times) {
 
 // converts free_times from min to string time format
 function freeTime(event_logs) {
-  busy = convertEvent(event_logs);
-  free_times = makeFree(busy);
-  free_intervals = freeIntervals(free_times);
-  times_str = makeTimesDict();
+  var busy = convertEvent(event_logs);
+  var free_times = makeFree(busy);
+  var free_intervals = freeIntervals(free_times);
+  var times_str = makeTimesDict();
 
-  free_times_str = [];
+  var free_times_str = [];
   for (let i = 0; i < free_intervals.length; i = i + 2) {
     free_times_str.push([
       times_str[free_intervals[i]],
@@ -123,9 +123,7 @@ function freeTime(event_logs) {
   return free_times_str;
 }
 
-// query database by day
-// run free time for each day
-
+/* TEST DATA
 var event_1 = ["5:00 PM", "6:15 PM"];
 var event_2 = ["2:00 AM", "2:30 PM"];
 var event_3 = ["10:00 PM", "11:55 PM"];
@@ -134,13 +132,70 @@ var event_log = [event_1, event_2, event_3];
 
 free_time_str = freeTime(event_log);
 
-console.log(free_time_str);
+console.log(free_time_str); */
 
-/* 
-export default function ScheduleTable() {
-    const { data } = useSWR("/api/events");
-  
-    return (
-        
-    );
-  } */
+function findFreeTime(data) {
+  // create dictionary with format 'day of week':'list of free time intervals'
+  var free_time_byDay = {};
+
+  var sun = [];
+  var mon = [];
+  var tues = [];
+  var wed = [];
+  var thurs = [];
+  var fri = [];
+  var sat = [];
+
+  if (typeof data === "object") {
+    for (let i = 0; i < data.length; i++) {
+      switch (data[i].eventday) {
+        case "Sunday":
+          sun.push([data[i].eventstarttime, data[i].eventendtime]);
+          break;
+        case "Monday":
+          mon.push([data[i].eventstarttime, data[i].eventendtime]);
+          break;
+        case "Tuesday":
+          tues.push([data[i].eventstarttime, data[i].eventendtime]);
+          break;
+        case "Wednesday":
+          wed.push([data[i].eventstarttime, data[i].eventendtime]);
+          break;
+        case "Thursday":
+          thurs.push([data[i].eventstarttime, data[i].eventendtime]);
+          break;
+        case "Friday":
+          fri.push([data[i].eventstarttime, data[i].eventendtime]);
+          break;
+        case "Saturday":
+          sat.push([data[i].eventstarttime, data[i].eventendtime]);
+      }
+    }
+  }
+  // evaluate free times on each day and store in dict
+  free_time_byDay["Sunday"] = freeTime(sun);
+  free_time_byDay["Monday"] = freeTime(mon);
+  free_time_byDay["Tuesday"] = freeTime(tues);
+  free_time_byDay["Wednesday"] = freeTime(wed);
+  free_time_byDay["Thursday"] = freeTime(thurs);
+  free_time_byDay["Friday"] = freeTime(fri);
+  free_time_byDay["Saturday"] = freeTime(sat);
+
+  return free_time_byDay;
+}
+
+export default function FreeTime() {
+  const { data } = useSWR("/api/events");
+  var free_time_byDay = findFreeTime(data);
+  return (
+    <div>
+      {Object.keys(free_time_byDay).map((key, index) => (
+        <p>
+          {" "}
+          You are free on {key} from {free_time_byDay[key][0][0]} to{" "}
+          {free_time_byDay[key][0][1]}
+        </p>
+      ))}
+    </div>
+  );
+}
