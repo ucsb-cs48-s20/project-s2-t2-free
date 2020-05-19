995bee4cc326bb8efe83b5a8494671e0dd686bbe
Vincent Tieu <vktieu@Vincents-MacBook-Pro.local>
2020-05-19 00:13:10 -0700

    vt - clean code

--- pages/my-group/[code]/index.js
+++ pages/my-group/[code]/index.js
@@ -12,29 +12,11 @@ export function GroupPage(props) {
   const user = props.user;
   const router = useRouter();
   const { code } = router.query;
-  const { data } = useSWR(`/api/groups/${code}`);
-  if (data === []) {
-    data = null;
-  }
+  let { data } = useSWR(`/api/groups/${code}`);
 
-  if (typeof data === "object" && data.length !== 0) {
-    return (
-      <Layout user={user}>
-        {data && (
-          <div>
-            <h1>Group Code: {code}</h1>
-            <JoinButton />
-            <MembersList />
-            <ScheduleTable />
-            <pre>{JSON.stringify(data, null, "\t")}</pre>
-          </div>
-        )}
-      </Layout>
-    );
-  }
   return (
     <Layout user={user}>
-      {data && (
+      {data && data[0] ? (
         <div>
           <h1>Group Code: {code}</h1>
           <JoinButton />
@@ -42,6 +24,8 @@ export function GroupPage(props) {
           <ScheduleTable />
           <pre>{JSON.stringify(data, null, "\t")}</pre>
         </div>
+      ) : (
+        <h1>Group does not exists</h1>
       )}
     </Layout>
   );
