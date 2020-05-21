0208e8f307147595bfd0f2f28a7ef2a07f59f1ed
Clare Boyle <claremboyle@ucsb.edu>
2020-05-20 20:38:06 -0700

    configuring react storybook

---
+++ stories/AppNavbar.stories.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { select, text } from "@storybook/addon-knobs";
+import AppNavbar from "../components/AppNavbar";
+
+export default {
+  title: "AppNavbar",
+  component: AppNavbar,
+};
+
+export const loggedOut = () => {
+  return <AppNavbar />;
+};
+
+export const loggedIn = () => {
+  const name = text("Name", "Phill Conrad");
+  const user = { name };
+  return <AppNavbar user={user} />;
+};
