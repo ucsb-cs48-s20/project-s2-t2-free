a64f34c51370f85010ea0b2b8d6604083ec7552e
Clare Boyle <claremboyle@ucsb.edu>
2020-05-28 19:21:06 -0700

    added knob for Layout story

--- stories/Layout.stories.js
+++ stories/Layout.stories.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Container from "react-bootstrap/Container";
+import { select, text } from "@storybook/addon-knobs";
 
 export default {
   title: "Layout",
   component: Layout,
 };
 
-export const layout = () => {
+export const loggedOutEmpty = () => {
   return <Layout />;
+};
+
+export const loggedInWithContentInContainer = () => {
+  const content = text("Sample Content", "This is sample content");
+  const name = text("Name", "Phill Conrad");
+  const user = { name };
+  return (
+    <Layout user={user}>
+      <Container className="py-3">{content}</Container>
+    </Layout>
+  );
 };
