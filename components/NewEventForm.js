a5c0defe2e3041cc9c997862033ee731da3a78e0
vincentktieu@ucsb.edu <vincentktieu@ucsb.edu>
2020-06-05 01:30:56 -0700

    vt - set default time

--- components/NewEventForm.js
+++ components/NewEventForm.js
@@ -8,24 +8,6 @@ import { useToasts } from "./Toasts";
 import { Accordion, Card } from "react-bootstrap";
 import { convertTime, numToTime } from "../utils/timeFuncs";
 
-function getTimeOptions(data) {
-  let timeOptions = [];
-  if (typeof data === "object" && data.length == 2) {
-    for (
-      let i = convertTime(data[0].endTime);
-      i <= convertTime(data[1].startTime);
-      i = i + 5
-    ) {
-      timeOptions.push(<option>{numToTime(i)}</option>);
-    }
-  } else {
-    for (let i = 0; i < 1339; i = i + 5) {
-      timeOptions.push(<option>{numToTime(i)}</option>);
-    }
-  }
-  return timeOptions;
-}
-
 function validateForm(
   e,
   isSunday,
@@ -77,7 +59,10 @@ function NewEventForm() {
   const [startTime, setStartTime] = useState("12:00 PM");
   const [endTime, setEndTime] = useState("12:00 PM");
 
-  const timeOptions = getTimeOptions(data);
+  const timeOptions = [];
+  for (let i = 0; i < 1439; i = i + 5) {
+    timeOptions.push(<option>{numToTime(i)}</option>);
+  }
 
   const addEvent = useCallback(
     async (e) => {
