0552915f65018bb704988d91114cf36aa9ea61c2
Clare Boyle <claremboyle@ucsb.edu>
2020-06-09 04:15:45 -0700

    cb - some grammatical changes

--- pages/index.js
+++ pages/index.js
@@ -21,21 +21,22 @@ function HomePage(props) {
         .
       </p>
       <p>
-        To get started with our app, login by navigating to the top right corner
-        of our home page and press our "Login" button.
+        To get started with our app, log in by navigating to the top right
+        corner of our home page and pressing our "Login" button.
       </p>
       <h2> My Schedule Page</h2>
       <p>The My Schedule page contains four main components:</p>
       <h5>1. Daily Start/End Times</h5>
       <p>
-        Allows you to set global start and end times to your calendar. For
-        example, you can set your default work hours from 9am to 5 pm each week.
+        Allows you to set global start and end times to your schedule. For
+        example, you can set your default work hours from 9 a.m. to 5 p.m. each
+        week.
       </p>
       <h5>2. Create New Event</h5>
       <p>
         Allows you to add events to your schedule. You can add events for
-        multiple days at a time and can select start and end times from our drop
-        down menu.
+        multiple days at a time and select start and end times from the
+        drop-down menus.
       </p>
       <h5>3. Schedule Table</h5>
       <p>
@@ -46,13 +47,13 @@ function HomePage(props) {
       <h5>4. Free Time</h5>
       <p>
         Text output of your available free time calculated from the daily
-        start/end times and all events that have been entered
+        start/end times and all events entered.
       </p>
 
       <h2> Groups Management Page </h2>
       <p>
         The Groups Management page allows you create and join groups through
-        shareable links.
+        shareable links and codes.
       </p>
       <h5>Creating Groups</h5>
       <p>
@@ -65,9 +66,9 @@ function HomePage(props) {
       <h5>Joining Groups</h5>
       <p>
         If you received a shareable link, navigate to the respective group page
-        and press "Join". If you received a unique group code, enter the code in
-        the Groups Management page which will redirect you to the group page
-        where you can press "Join".
+        and press "Join". If you received a unique group code, entering the code
+        on the Groups Management page will redirect you to the group page where
+        you can press "Join".
       </p>
 
       <h2> My Groups </h2>
